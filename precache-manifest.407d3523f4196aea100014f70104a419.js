91a06729d780ab0378ca6d671d8c24ec5ffecf42
Daniel Garza <danielg@Daniels-MacBook-Pro.local>
2019-09-22 22:03:48 -0500

    Updates

---
+++ precache-manifest.407d3523f4196aea100014f70104a419.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35fa2267ae52d25c6fa30922ae8ce022",
+    "url": "/DanielG/my-app/public/index.html/index.html"
+  },
+  {
+    "revision": "0ab659622b527ad8cfb2",
+    "url": "/DanielG/my-app/public/index.html/static/css/main.9dfec8cf.chunk.css"
+  },
+  {
+    "revision": "44615bf0aa314f9c4216",
+    "url": "/DanielG/my-app/public/index.html/static/js/2.d998281c.chunk.js"
+  },
+  {
+    "revision": "0ab659622b527ad8cfb2",
+    "url": "/DanielG/my-app/public/index.html/static/js/main.05871836.chunk.js"
+  },
+  {
+    "revision": "5a4a401489931d08679e",
+    "url": "/DanielG/my-app/public/index.html/static/js/runtime~main.35517219.js"
+  },
+  {
+    "revision": "8f79bd9c0a4d13faef94e33d3cee3b66",
+    "url": "/DanielG/my-app/public/index.html/static/media/AustinPic.8f79bd9c.png"
+  },
+  {
+    "revision": "b8b83e0eb1f92a7413eb22092fb83102",
+    "url": "/DanielG/my-app/public/index.html/static/media/THD_logo.b8b83e0e.jpg"
+  },
+  {
+    "revision": "712421aa2a5855ddddc95c9f85834cd6",
+    "url": "/DanielG/my-app/public/index.html/static/media/myFace2.712421aa.jpg"
+  },
+  {
+    "revision": "ae54a1dd4617d6b461ba2577c3b3aa4e",
+    "url": "/DanielG/my-app/public/index.html/static/media/myFace4.ae54a1dd.png"
+  }
+]);
