82cc1642db19abab53dae9d517e4deb90d371700
Daniel Garza <danielg@Daniels-MacBook-Pro.local>
2019-10-12 18:45:21 -0500

    Updates

---
+++ precache-manifest.d9e6d586b0e897d764241ef9f183b0bb.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025891087cc6a128df5f4c33efa77a22",
+    "url": "/DanielG/index.html"
+  },
+  {
+    "revision": "ae6827106c47a21cd241",
+    "url": "/DanielG/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "648f14fa848ba8672815",
+    "url": "/DanielG/static/css/main.7148e1bb.chunk.css"
+  },
+  {
+    "revision": "ae6827106c47a21cd241",
+    "url": "/DanielG/static/js/2.367d8295.chunk.js"
+  },
+  {
+    "revision": "648f14fa848ba8672815",
+    "url": "/DanielG/static/js/main.bc7e0b00.chunk.js"
+  },
+  {
+    "revision": "fc529bcb0ca6189cfe75",
+    "url": "/DanielG/static/js/runtime~main.e8b12e7c.js"
+  },
+  {
+    "revision": "b8b83e0eb1f92a7413eb22092fb83102",
+    "url": "/DanielG/static/media/THD_logo.b8b83e0e.jpg"
+  },
+  {
+    "revision": "0759fde862b3d7db72aaa2a86a6bfd68",
+    "url": "/DanielG/static/media/UT2.0759fde8.png"
+  },
+  {
+    "revision": "4954ce07ee75489ca5c81c63b4730c0c",
+    "url": "/DanielG/static/media/cat1.4954ce07.jpg"
+  },
+  {
+    "revision": "4279a2fb11b67407ebb49026c047123d",
+    "url": "/DanielG/static/media/cat2.4279a2fb.jpg"
+  },
+  {
+    "revision": "b0fca43d4d2cdf27d47eeca626a71aff",
+    "url": "/DanielG/static/media/cat3.b0fca43d.jpg"
+  },
+  {
+    "revision": "7eee713d7439bfdb634fc35e50954a5f",
+    "url": "/DanielG/static/media/cat4.7eee713d.jpg"
+  },
+  {
+    "revision": "a9e5b80ada428bd97990761d1d7b5324",
+    "url": "/DanielG/static/media/cat5.a9e5b80a.jpg"
+  },
+  {
+    "revision": "fefa9250a1f1f0b86230cae3c08495f6",
+    "url": "/DanielG/static/media/cat6.fefa9250.jpg"
+  },
+  {
+    "revision": "480a90f1c3abeec7c4fb2baafceb6d14",
+    "url": "/DanielG/static/media/headerBackground.480a90f1.jpg"
+  },
+  {
+    "revision": "653f0625ec783f5ea68e995d9b3b9e9a",
+    "url": "/DanielG/static/media/myFace3.653f0625.png"
+  },
+  {
+    "revision": "8bd855a09d56d1ad8713e5e866b7b4ac",
+    "url": "/DanielG/static/media/resume.8bd855a0.pdf"
+  },
+  {
+    "revision": "b72b30ee238e3ba67719ab5a8dac91cf",
+    "url": "/DanielG/static/media/tech2.b72b30ee.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DanielG/precache-manifest.e476b2bcf313f5f55e757e78935c9943.js"
+  "/DanielG/precache-manifest.d9e6d586b0e897d764241ef9f183b0bb.js"
 );
 
 self.addEventListener('message', (event) => {
