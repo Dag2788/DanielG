64899dad83d723521ed0b24dd7063db712be1164
dag3733 <daniel_a_garza@homedepot.com>
2019-10-11 21:53:39 -0500

    final touches mobile

--- my-app/src/body/content/techStack.js
+++ my-app/src/body/content/techStack.js
@@ -10,79 +10,79 @@ function TechStack() {
         </h1>
       </div>
       <div class="row paddingTopSmall textAlignCenter stackDivisor">
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Javascript
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           ES6
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           HTML
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           CSS
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           JQuery
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Node
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Angular
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           React
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Redux
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Jest
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Enzyme
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Flow
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Webpack
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Grunt
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Babel
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Typescript
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Axios
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Thunk
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Sagas
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           MomentJs
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Materialize
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Lodash
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Jasmine
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Selenium
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Puppeteer
         </h4>
       </div>
--- my-app/src/body/content/techStackBed.js
+++ my-app/src/body/content/techStackBed.js
@@ -10,55 +10,55 @@ function TechStackBed() {
         </h1>
       </div>
       <div class="row  paddingTopSmall">
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Java
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           C++
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Python
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Node
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Maven
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           REST
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           AspectJ
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Cucumber
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           TestNG
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Swagger
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Zipkin
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           XML
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           SQL
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Perl
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Linux
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Git
         </h4>
-        <h4 class="col s4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
+        <h4 class="col s6 l4 waves-effect waves-light fontTinos buttonFont textAlignCenter">
           Spring Boot
         </h4>
       </div>
